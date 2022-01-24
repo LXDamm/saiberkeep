36b2e4046f33c7c678be7e6cdd3bd7767362322c
Lexx Damm <lexx.damm@gmail.com>
2022-01-24 19:13:51 +0100

    from example

--- saiberkeep-client/public/index.js
+++ saiberkeep-client/public/index.js
@@ -1,27 +1,22 @@
-const statusElm = document.getElementById('status');
+const scanButton = document.getElementById('scan-button');
 
-const init = () => {
-  if (window.isSecureContext) {
-    statusElm.innerText = 'Secure context';
-  } else {
-    statusElm.innerText = 'No secure context';
+scanButton.addEventListener("click", async () => {
+  log("User clicked scan button");
+
+  try {
+    const ndef = new NDEFReader();
+    await ndef.scan();
+    log("> Scan started");
+
+    ndef.addEventListener("readingerror", () => {
+      log("Argh! Cannot read data from the NFC tag. Try another one?");
+    });
+
+    ndef.addEventListener("reading", ({ message, serialNumber }) => {
+      log(`> Serial Number: ${serialNumber}`);
+      log(`> Records: (${message.records.length})`);
+    });
+  } catch (error) {
+    log("Argh! " + error);
   }
-
-  const reader = new NDEFReader();
-  let read = reader.scan();
-  read.then(event => {
-    statusElm.innerText = 'Scan started successfully.';
-
-    ndef.onreadingerror = event => {
-      statusElm.innerText = 'Error! Cannot read data from the NFC tag. Try a different one?';
-    };
-
-    ndef.onreading = event => {
-      statusElm.innerText = 'NDEF message read.';
-    };
-  }).catch(error => {
-    statusElm.innerText = `Error! Scan failed to start: ${error}.`;
-  });
-};
-
-init();
+});
