5ab1fe80d900477aa40edcfdf2f502890e98f6c1
Lexx Damm <lexx.damm@gmail.com>
2022-01-25 23:11:11 +0100

    ...

--- saiberkeep-client/public/index.js
+++ saiberkeep-client/public/index.js
@@ -7,6 +7,7 @@ const ndefInit = async (ndef) => {
 }
 
 const ndefSyncRead = async (ndef) => {
+  const records = [];
   try {
     await ndef.scan();
     nfcStatusP.innerText = 'Sync read started...';
@@ -17,18 +18,23 @@ const ndefSyncRead = async (ndef) => {
 
     ndef.addEventListener("reading", ({ message, serialNumber }) => {
       nfcStatusP.innerText = 'Reading records...';
+      if (message.records.length) {
       message.records.forEach(record => {
         if (record.recordType === 'text') {
           const textDecoder = new TextDecoder(record.encoding);
-
+          records.push(textDecoder(record));
         }
       });
+      } else {
+        nfcStatusP.innerText = 'No NDEF records on implant/card';
+      }
       html = html + '</ul>';
       nfcStatusP.innerHTML = html;
     });
   } catch (error) {
     nfcStatusP.innerText = 'Error: ' + error;
   }
+  return records;
 };
 
 export { ndefInit, ndefSyncRead };
