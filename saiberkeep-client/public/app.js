4a1e53ac497ffdf487c5e865eee413b7463c5f2f
Lexx Damm <lexx.damm@gmail.com>
2022-01-25 23:53:53 +0100

    ...

--- saiberkeep-client/public/app.js
+++ saiberkeep-client/public/app.js
@@ -1,4 +1,5 @@
-const syncButtonElm = document.getElementById('sync-button');class App {
+const syncButtonElm = document.getElementById('sync-button');
+class App {
     ndef = undefined;
     records = [];
     constructor(ndef) {
@@ -7,38 +8,36 @@ const syncButtonElm = document.getElementById('sync-button');class App {
     setText = (text) => {
         const statusTextElm = document.getElementById('nfc-status');
         statusTextElm.innerText = text;
-    }
+    };
     run = () => {
         syncButtonElm.addEventListener('click', () => {
             this.sync();
         });
-    }
+    };
     sync = async () => {
         this.setText('Beginning sync...');
         this.syncRead();
-    }
+    };
     syncRead = async () => {
         await this.ndef.scan();
-        this.ndef.addEventListener("readingerror", () => {
+        this.ndef.addEventListener('readingerror', () => {
             this.setText('Error reading data from implant/card');
         });
-        this.ndef.addEventListener("reading", ({ message, serialNumber }) => {
+        this.ndef.addEventListener('reading', ({ message, serialNumber }) => {
             this.setText('NDEF data found, reading records...');
             if (message.records.length) {
-                message.records.forEach(record => {
-                  if (record.recordType === 'text') {
-                    const textDecoder = new TextDecoder(record.encoding);
-                    records.push(textDecoder(record));
-                  }
+                message.records.forEach((record) => {
+                    if (record.recordType === 'text') {
+                        const textDecoder = new TextDecoder(record.encoding);
+                        this.records.push(textDecoder(record));
+                    }
                 });
-                } else {
-                  this.setText('Zero NDEF records on implant/card');
-                }
+            } else {
+                this.setText('Zero NDEF records on implant/card');
+            }
         });
-    }
-    syncWrite = async () => {
-
-    }
+    };
+    syncWrite = async () => {};
 }
 
 export default App;
