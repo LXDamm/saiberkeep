00e49f7d1688ed08cc7b65eaba856e76f2ff8579
Lexx Damm <lexx.damm@gmail.com>
2022-02-02 14:37:28 +0100

    ...

--- saiberkeep-client/public/app.js
+++ saiberkeep-client/public/app.js
@@ -24,41 +24,8 @@ class App {
         });
     };
     sync = async () => {
-        this.setText('Beginning sync...');
-        this.syncRead();
-        document.addEventListener('syncread', () => {
-            this.setText('Records read and saved');
-            this.records.forEach(record => {
-                console.log(record);
-            });
-        });
     };
     syncRead = async () => {
-        this.syncing = true;
-        await this.ndef.scan();
-        this.ndef.addEventListener('readingerror', () => {
-            this.setText('Error reading data from implant/card');
-            this.syncing = false;
-        });
-        this.ndef.addEventListener('reading', ({ message, serialNumber }) => {
-            this.setText('NDEF data found, reading records...');
-            if (message.records.length) {
-                message.records.forEach((record) => {
-                    if (record.recordType === 'text') {
-                        this.records.push(this.textDecoder.decode(record.data));
-                        localStorage.setItem('records', JSON.stringify(this.records));
-                    } else if (record.recordType === 'mime') {
-                        if (this.records.mediaType === 'application/json') {
-                            console.log(JSON.parse(this.decoder.decode(record.data)));
-                        }
-                    }
-                });
-                document.dispatchEvent(syncReadEvent);
-            } else {
-                this.setText('Zero NDEF records on implant/card');
-            }
-            this.syncing = false;
-        });
     };
     syncWrite = async () => {
         await this.ndef.scan();
